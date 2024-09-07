18f48799ebf22998c420c300905eb4d4c4339ca9
Houider <houiderwalid@gmail.com>
2024-09-07 14:54:02 +0100

    add image og

--- wp-content/themes/LetsGoSimo/header.php
+++ wp-content/themes/LetsGoSimo/header.php
@@ -19,6 +19,7 @@ $phone = $options['phone_number'] ?? '';
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet">
+    <meta property="og:image" content="https://houiderwalid.com/wp-content/uploads/2024/08/morocco-8197176_1920.jpg">
     <title><?php echo $title ?></title>
     <meta name="description" content="<?php echo $description ?>">
     <meta name="keywords" content="<?php echo $keywords ?>">
